09126c9ed34d3ba0c197b6294fbc9354fe86923d
Santiago Rodriguez <rodrigueznohsantiagomiguel@gmail.com>
2024-04-29 18:16:21 -0600

    feature: update app generator, add loading animation and move logo to the Generator class

--- src/AppGenerator.mjs
+++ src/AppGenerator.mjs
@@ -2,50 +2,40 @@ import _ from "lodash";
 import enquirer from "enquirer";
 import * as fs from "fs/promises";
 import path from "path";
-import os from 'os'
-import url from 'url';
+import os from "os";
+import url from "url";
 import { generatePackageJson } from "./generators/app/package.json.template.js";
 import { generateDockerCompose } from "./generators/app/docker-compose.yml.template.js";
 import { generateDockerComposeEnv } from "./generators/app/.docker-compose.env.template.js";
-import {generateIndex} from "./generators/app/src/config/index.template.js"
+import { generateIndex } from "./generators/app/src/config/index.template.js";
 import { generateEnvExample } from "./generators/app/env.example.template.js";
 import { createRequire } from "module";
 import chalk from "chalk";
 import { spawnSync } from "child_process";
 import boxen from "boxen";
 import { Generator } from "./Generator.mjs";
-
-const require = createRequire(import.meta.url);
-const pjson = require("../package.json");
-
+import ora from "ora";
+import promiseSpawn from "@npmcli/promise-spawn";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-export class AppGenerator extends Generator{
-   appName;
-   defaultAuthor;
-   prompts;
-   destinationPath;
-   originPath;
-   responses;
-   logo=`   
-    ______ __    __  __ ______ _   __ ______ _____ ______
-   / ____// /   / / / // ____// | / // ____// ___//_  __/
-  / /_   / /   / / / // / __ /  |/ // __/   \\__ \\  / /   
- / __/  / /___/ /_/ // /_/ // /|  // /___  ___/ / / /    
-/_/    /_____/\\____/ \\____//_/ |_//_____/ /____/ /_/  
-v${pjson.version}
-`
+export class AppGenerator extends Generator {
+  appName;
+  defaultAuthor;
+  prompts;
+  destinationPath;
+  originPath;
+  responses;
 
   constructor(projectName) {
     super();
     this.appName = _.kebabCase(projectName);
-    this.defaultAuthor =os.userInfo().username;
+    this.defaultAuthor = os.userInfo().username;
     this.prompts = [
       {
         type: "input",
         name: "name",
-        message: "Your project name",
+        message: "Your project name:",
         initial: this.appName,
       },
       {
@@ -77,74 +67,71 @@ v${pjson.version}
     ];
   }
   async run() {
-    console.log( chalk.green(this.logo) + "\nWelcome to the " + chalk.red("flugnest") + " generator\n")
-    const responses= await enquirer.prompt(this.prompts);
-    this.responses=responses;
-    this.destinationPath =process.cwd()+"/"+this.appName;
-    this.originPath= __dirname+'/generators/app'
+    console.log(
+      chalk.green(this.logo) + "\nWelcome to the " + chalk.red("flugnest") + " generator\n",
+    );
+    const responses = await enquirer.prompt(this.prompts);
+    this.responses = responses;
+    this.destinationPath = process.cwd() + "/" + this.appName;
+    this.originPath = __dirname + "/generators/app";
     try {
-      await fs.mkdir( path.resolve(this.destinationPath));
+      await fs.mkdir(path.resolve(this.destinationPath));
 
-      await this.copyResources( this.originPath,this.destinationPath)
+      await this.copyResources(this.originPath, this.destinationPath);
       await Promise.all([
         this.copyPackageJson(responses),
         this.copyEnvExample(responses),
         this.copyDockerCompose(responses),
-        this.copyIndex(responses)
-      ]) 
-      this.install()
-      this.end()
+        this.copyIndex(responses),
+      ]);
+      await this.install();
+      await this.end();
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
-
   }
 
-
-
-  async copyPackageJson(data){
-    const template=generatePackageJson(data)
-    await this.saveFile(this.destinationPath,'package.json',template)
+  async copyPackageJson(data) {
+    const template = generatePackageJson(data);
+    await this.saveFile(this.destinationPath, "package.json", template);
   }
-  async copyEnvExample(data){
-    const template= generateEnvExample(data)
-    await this.saveFile(this.destinationPath,'.env.example',template)
+  async copyEnvExample(data) {
+    const template = generateEnvExample(data);
+    await this.saveFile(this.destinationPath, ".env.example", template);
   }
-  async copyDockerCompose(data){
-    const template =generateDockerCompose(data)
-    await this.saveFile(this.destinationPath,'docker-compose.yml',template)
+  async copyDockerCompose(data) {
+    const template = generateDockerCompose(data);
+    await this.saveFile(this.destinationPath, "docker-compose.yml", template);
   }
-  async copyDockerComposeEnv(data){
-    const template =generateDockerComposeEnv(data)
-    await this.saveFile(this.destinationPath,'docker-compose.env',template)
+  async copyDockerComposeEnv(data) {
+    const template = generateDockerComposeEnv(data);
+    await this.saveFile(this.destinationPath, "docker-compose.env", template);
   }
-  async copyIndex(data){
-    const template = generateIndex(data)
-    await this.saveFile(this.destinationPath,'src/config/index.ts',template)
+  async copyIndex(data) {
+    const template = generateIndex(data);
+    await this.saveFile(this.destinationPath, "src/config/index.ts", template);
   }
 
-  end(){
-    const spawnConfig={cwd:this.destinationPath,shell:true}
-    
-    console.time('\n'+chalk.green('Formatting Files')); 
-    spawnSync('npm',['run','format'],spawnConfig)
-    console.timeEnd('\n'+chalk.green('Formatting Files')); 
+  async end() {
+    const spawnConfig = { cwd: this.destinationPath, shell: true, stdio: "ignore" };
 
-    console.time('\n'+chalk.green('Setting up Git')); 
-    spawnSync("git", ["init", "--quiet"],spawnConfig);
-    spawnSync("git", ["add", "."],spawnConfig);
-    spawnSync("git", ["commit", "-m", "Initial Project Setup"],spawnConfig);
-    console.timeEnd('\n'+chalk.green('Setting up Git')); 
-
+    const formattingFilesSpinner = ora(chalk.green("Formatting Files ...")).start();
+    await promiseSpawn("npm", ["run", "format"], spawnConfig);
+    formattingFilesSpinner.succeed();
+    const settingUpGitSpinner = ora(chalk.green("Setting up Git ...")).start();
+    await promiseSpawn("git", ["init", "--quiet"], spawnConfig);
+    await promiseSpawn("git", ["add", "."], spawnConfig);
+    await promiseSpawn("git", ["commit", "-m", "Initial Project Setup"], spawnConfig);
+    settingUpGitSpinner.succeed();
+    console.log("\n");
     const content = `${chalk.green("Your project is ready!")}
 For instructions on how to get started, please see README.md
 Run it with: \n
-${chalk.blue("cd "+this.appName)}
+${chalk.blue("cd " + this.appName)}
 ${chalk.blue("npm run test")}
 ${chalk.blue("npm run start:dev")}
     `;
-      const msg = boxen(content, { padding: 1, borderStyle: "round" });
-      console.log(msg);
-    }
-
+    const msg = boxen(content, { padding: 1, borderStyle: "round" });
+    console.log(msg);
+  }
 }
--- src/Generator.mjs
+++ src/Generator.mjs
@@ -1,39 +1,57 @@
 import { glob } from "glob";
 import path from "path";
 import * as fs from "fs/promises";
+import { createRequire } from "module";
 import chalk from "chalk";
 import { spawnSync } from "child_process";
-export class Generator{
-    async copyResources(srcDir, destDir, opts={ ignore: "**/*.template.js"}) {
-        // Use glob to get a list of all the files in the source directory
-        const files = await glob( [srcDir+'/**/*',srcDir+'/.*'],opts);
-        // console.log(files)
-        for (const srcPath of files) {
-          // Construct the full path to the destination file
-          const destPath =path.resolve(path.resolve(srcPath).replace(path.resolve(srcDir), path.resolve(destDir)));
-          // Check if the file is a directory
-          const stats = await fs.stat(srcPath);
-    
-          if (stats.isDirectory()) {
-            // If the file is a directory, create it in the destination
-            await fs.mkdir(destPath, { recursive: true });
-          } else {
-            // If the file is a regular file, copy it
-            await fs.copyFile(srcPath, destPath);
-          }
-        }
-        
-      }
-    
-      async saveFile(dir,name, template) {
-        const filename = path.join(dir, name);
-        await fs.writeFile(filename, template);
-      };
+import promiseSpawn from "@npmcli/promise-spawn";
+import ora from "ora";
+const require = createRequire(import.meta.url);
+const pjson = require("../package.json");
 
-      install(){
-        const spawnConfig={cwd:this.destinationPath,stdio:'inherit',shell:true}
-        console.log('\n'+chalk.green('Installing dependencies \n '))
-        spawnSync('npm',['i'],spawnConfig)
-    
+export class Generator {
+  logo = `   
+    ______ __    __  __ ______ _   __ ______ _____ ______
+   / ____// /   / / / // ____// | / // ____// ___//_  __/
+  / /_   / /   / / / // / __ /  |/ // __/   \\__ \\  / /   
+ / __/  / /___/ /_/ // /_/ // /|  // /___  ___/ / / /    
+/_/    /_____/\\____/ \\____//_/ |_//_____/ /____/ /_/  
+v${pjson.version}
+`;
+
+  async copyResources(srcDir, destDir, opts = { ignore: "**/*.template.js" }) {
+    // Use glob to get a list of all the files in the source directory
+    const files = await glob([srcDir + "/**/*", srcDir + "/.*"], opts);
+    // console.log(files)
+    for (const srcPath of files) {
+      // Construct the full path to the destination file
+      const destPath = path.resolve(
+        path.resolve(srcPath).replace(path.resolve(srcDir), path.resolve(destDir)),
+      );
+      // Check if the file is a directory
+      const stats = await fs.stat(srcPath);
+
+      if (stats.isDirectory()) {
+        // If the file is a directory, create it in the destination
+        await fs.mkdir(destPath, { recursive: true });
+      } else {
+        // If the file is a regular file, copy it
+        await fs.copyFile(srcPath, destPath);
       }
+    }
+  }
+
+  async saveFile(dir, name, template) {
+    const filename = path.join(dir, name);
+    await fs.writeFile(filename, template);
+  }
+
+  async install() {
+    const spawnConfig = { cwd: this.destinationPath, stdio: "pipe", shell: true };
+    console.log("\n\n");
+    const formattingFilesSpinner = ora(chalk.green("Installing dependencies") + "\n").start();
+    const result = await promiseSpawn("npm", ["i"], spawnConfig);
+    formattingFilesSpinner.succeed();
+    console.log(result.stdout + "\n");
+  }
 }
