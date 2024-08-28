fc6b752408059ce6beb31d989c573b6103d9541c
Santiago Rodriguez <santiagomrn98@gmail.com>
2024-08-28 08:56:30 -0600

    fix(queue): correct typo in function names, use the correct logger and listen to the correct queue

--- src/generators/queueModule/queue.controller.ts
+++ src/generators/queueModule/queue.controller.ts
@@ -1,69 +1,64 @@
-import { Controller, Get, Inject, Injectable, Post } from '@nestjs/common';
-import { Payload, Ctx } from '@nestjs/microservices';
-import { ServiceBusMessage, ServiceBusReceiver } from '@azure/service-bus';
+import { Controller, Get, Inject, Injectable, Post } from "@nestjs/common";
+import { Payload, Ctx } from "@nestjs/microservices";
+import { ServiceBusMessage, ServiceBusReceiver } from "@azure/service-bus";
 import {
   AzureServiceBusClientProxy,
   AzureServiceBusContext,
   Queue,
   Topic,
-} from 'nestjs-azure-service-bus-transporter';
-import { config } from '@config/index';
-import { ApiTags } from '@nestjs/swagger';
+} from "nestjs-azure-service-bus-transporter";
+import { config } from "@config/index";
+import { ApiTags } from "@nestjs/swagger";
+import { Logger } from "@core/logger/Logger";
 
-@ApiTags('servicebus')
-@Controller('servicebus')
+@ApiTags("servicebus")
+@Controller("servicebus")
 export class QueueController {
-  constructor(
-    @Inject('SB_CLIENT') private readonly sbClient: AzureServiceBusClientProxy,
-  ) {}
-  @Get('queue/emit')
+  private logger: Logger = new Logger(QueueController.name);
+  constructor(@Inject("SB_CLIENT") private readonly sbClient: AzureServiceBusClientProxy) {}
+
+  @Get("queue/emit")
   queueEmit(): string {
     this.sbClient.emit(config.azure.serviceBus.queueExample, {
       body: {
-        test: 'queue',
+        test: "queue",
       },
     });
-    return 'ok';
+    return "ok";
   }
 
-  @Get('topic/emit')
+  @Get("topic/emit")
   topicEmit(): string {
-    this.sbClient.emit(config.azure.serviceBus.queueExample, {
+    this.sbClient.emit(config.azure.serviceBus.topicExample, {
       body: {
-        test: 'topic',
+        test: "topic",
       },
     });
-    return 'ok';
+    return "ok";
   }
 
   @Queue({
     queueName: config.azure.serviceBus.queueExample.name,
-    receiveMode: 'peekLock',
+    receiveMode: "peekLock",
     options: {
       autoCompleteMessages: true,
     },
   })
-  handleQueuemessage(
-    @Payload() data: ServiceBusMessage,
-    @Ctx() context: AzureServiceBusContext,
-  ) {
+  handleQueueMessage(@Payload() data: ServiceBusMessage, @Ctx() context: AzureServiceBusContext) {
     const serviceBusReceiver: ServiceBusReceiver = context.getArgs()[0];
-    console.log(data.body);
+    this.logger.info(data.body);
   }
 
   @Topic({
     topic: config.azure.serviceBus.topicExample.name,
     subscription: config.azure.serviceBus.topicExample.subscription,
-    receiveMode: 'peekLock',
+    receiveMode: "peekLock",
     options: {
       autoCompleteMessages: true,
     },
   })
-  handleTopicmessage(
-    @Payload() data: ServiceBusMessage,
-    @Ctx() context: AzureServiceBusContext,
-  ) {
+  handleTopicMessage(@Payload() data: ServiceBusMessage, @Ctx() context: AzureServiceBusContext) {
     const serviceBusReceiver: ServiceBusReceiver = context.getArgs()[0];
-    console.log(data.body);
+    this.logger.info(data.body);
   }
 }
